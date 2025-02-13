c0887f8d63941ce819d6aae128ea0a27c764e41c
bitc0index <ma3160142@gmail.com>
2025-02-13 22:07:35 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  amountToWrap: 0.0001, // use 0.0001 WPOL to tPOL
+  autoRestartDelay: 10, // delay in seconds before executing another swap
+  repeat: 20 // repeat swap 10 times (default)
+}
